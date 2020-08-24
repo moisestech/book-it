d64eee659a9de4130b7e7265a275f8ff1d33d4b0
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-24 09:49:41 -0400

    functional root component wuth useState and useEffect to create MyBooks from BookAPI

--- app/components/BookIt.js
+++ app/components/BookIt.js
@@ -1,8 +1,64 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import { debounce } from 'throttle-debounce'
+import * as Books API from '../utils/BooksAPI'
+// import ListBooks from './ListBooks'
+// import SearchBooks from './SearchBooks'
+
+
+const bookshelves = [
+  { key: 'currentlyReading', name: 'Currently Reading' },
+  { key: 'wantToRead', name: 'Want to Read' },
+  { key: 'read', name: 'Read' }
+]
 
 export default function Bookit () {
+  const [myBooks, setMyBooks ] = React.useState([]),
+  const [searchBooks, setSearchBooks] = React.useState([])
+  const [error, setError] = React.useState(false)
+
+  React.useEffect(() => {
+    BooksAPI.getAll()
+      .then(books => setMyBooks(books))
+      .catch(err => {
+        console.log(err)
+        setState(true)
+      })
+  }, [myBooks, error])
+
+  React.useEffect(() => {
+
+  }, [searchBooks])
+
+  React.useEffect(() => {
+
+  }, [setSearchBooks, error])
+
 
   return (
-    <div>Book It</div>
+    <div className="app">
+        <Route
+          exact path="/"
+          component={
+            <ListBooks
+              bookshelves={bookshelves}
+              books={myBooks}
+              onMove={this.moveBook}
+            />
+          }
+        />
+        <Route
+          path="/search"
+          component={
+            <SearchBooks
+              searchBooks={searchBooks}
+              myBooks={myBooks}
+              onSearch={this.searchForBooks}
+              onMove={this.moveBook}
+              onResetSearch={this.resetSearch}
+            />
+          }
+        />
+    </div>
   )
 }
