16575f2a684ad620bd11e5fed57ea473ca53184a
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-24 05:03:17 -0400

    functional component taking in 3 props rendering bookshelves

--- app/components/ListBooks.js
+++ app/components/ListBooks.js
@@ -1,1 +1,35 @@
-ListBooks.js
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import Bookshelf from './Bookshelf'
+
+export default ListBooks ({ bookshelves, books, onMove }) {
+  return (
+    <div className='list-books'>
+      <div className='list-books-title'>
+        <h1>BookIT</h1>
+      </div>
+      <div className='list-books-content' >
+        {bookshelves.map(shelf => (
+          <Bookshelf
+            key={shelf.key}
+            shelf={shelf}
+            books={books}
+            onMove={onMove}
+          />
+        ))}
+      </div>
+      <div className='open-search'>
+        <Link to="search">
+          <button>Add a Book</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+ListBooks.PropTypes = {
+  bookshelves: PropTypes.object.isRequired,
+  books: PropTypes.array.isRequired,
+  onMove: PropsTypes.function.isRequired
+}
