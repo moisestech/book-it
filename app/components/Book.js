8ff4c7009ff20b0b5673851886aa494d92a0f17f
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-25 07:04:33 -0400

    functional component rendering book obj

--- app/components/Book.js
+++ app/components/Book.js
@@ -1,1 +1,38 @@
-Book.js
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Book ({ book, shelf, onMove }) {
+
+  return (
+    <li>
+      <div className='book'>
+        <div className='book-top'>
+          <div
+            className='book-cover'
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${
+                book.imageLinks
+                  ? book.imageLinks.thumbnail
+                  : 'icons/book-placeholder.svg'
+              })`
+            }}
+          />
+        </div>
+        <div className='book-title'>
+          {book.title}
+        </div>
+        <div className='book-authors'>
+          {book.authors ? book.authors.join(', '): 'Unknown Author'}
+        </div>
+      </div>
+    </li>
+  )
+}
+
+Book.propTypes = {
+  book: PropTypes.object,
+  shelf: PropTypes.string,
+  // onMove: PropTypes.function
+}
