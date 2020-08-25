198752a643f503489e968b61ef4e4984ed6d69fd
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-25 05:06:23 -0400

    refactoring to useEffect, useReducer, and useState

--- app/components/BookIt.js
+++ app/components/BookIt.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { debounce } from 'throttle-debounce'
-import * as Books API from '../utils/BooksAPI'
+import * as BooksAPI from '../utils/BooksAPI'
 // import ListBooks from './ListBooks'
 // import SearchBooks from './SearchBooks'
 
@@ -12,53 +12,75 @@ const bookshelves = [
   { key: 'read', name: 'Read' }
 ]
 
+function myBooksReducer (state, action) {
+  if (action.type === 'success') {
+    return {
+      ...state,
+      myBooks: action.books,
+      error: false
+    }
+  } else if (action.type === 'error') {
+    return {
+      ...state,
+      error: true
+    }
+  } else {
+      throw new Error(`'That action type isn't supported.`)
+  }
+}
+
 export default function Bookit () {
-  const [myBooks, setMyBooks ] = React.useState([]),
+  const [myBooks, setMyBooks] = React.useState([])
   const [searchBooks, setSearchBooks] = React.useState([])
-  const [error, setError] = React.useState(false)
+  const [state, dispatch] = React.useReducer(
+    myBooksReducer,
+    { error: null}
+  )
 
   React.useEffect(() => {
     BooksAPI.getAll()
-      .then(books => setMyBooks(books))
-      .catch(err => {
-        console.log(err)
-        setState(true)
-      })
-  }, [myBooks, error])
+      .then((books) => dispatch({ type: 'success', books}))
+      .catch((error) => dispatch({ type: 'error', error }))
+  }, [myBooks])
 
-  React.useEffect(() => {
 
-  }, [searchBooks])
 
-  React.useEffect(() => {
+  // React.useEffect(() => {
 
-  }, [setSearchBooks, error])
+  // }, [setSearchBooks])
 
 
   return (
-    <div className="app">
-        <Route
-          exact path="/"
-          component={
-            <ListBooks
-              bookshelves={bookshelves}
-              books={myBooks}
-              onMove={this.moveBook}
-            />
-          }
-        />
-        <Route
-          path="/search"
-          component={
-            <SearchBooks
-              searchBooks={searchBooks}
-              myBooks={myBooks}
-              onSearch={this.searchForBooks}
-              onMove={this.moveBook}
-              onResetSearch={this.resetSearch}
-            />
-          }
-        />
-    </div>
+    <>
+      {state.error && <div>Network error. Please try again later.</div>}
+      <div className="app">
+
+          <div>List Books</div>
+          <div>Search Books</div>
+
+          {/*<Route
+            exact path="/"
+            component={
+              <ListBooks
+                bookshelves={bookshelves}
+                books={myBooks}
+                onMove={this.moveBook}
+              />
+            }
+          />
+          <Route
+            path="/search"
+            component={
+              <SearchBooks
+                searchBooks={searchBooks}
+                myBooks={myBooks}
+                onSearch={this.searchForBooks}
+                onMove={this.moveBook}
+                onResetSearch={this.resetSearch}
+              />
+            }
+          />*/}
+      </div>
+    </>
   )
 }
