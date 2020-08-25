5eb031355559db8a05fe4c5fed23bc264160b9de
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-25 09:25:13 -0400

    updated useState with shelfState, and setShelfState

--- app/components/BookshelfChanger.js
+++ app/components/BookshelfChanger.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import Proptypes from 'prop-types'
+import PropTypes from 'prop-types'
 
 export default function BookshelfChanger ({ book, shelf, onMove }) {
-  const [shelf, setShelf] = React.useState('')
+  const [shelfState, setShelfState] = React.useState('')
 
   const handleChange = (event) => {
     const { value } = event.target
-    setShelf(value)
+    setShelfState(value)
   }
 
   return (
     <div className='book-shelf-changer'>
       <select
-        value={state.shelf}
+        value={shelfState.shelf}
         onChange={(event) => handleChange(event)}
       >
         <option value='move' disabled>
