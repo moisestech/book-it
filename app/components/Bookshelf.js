9cafc7a893638534163f82af6524f4fdd2435e94
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-25 07:08:51 -0400

    functional component filtering books array to render current books on each shelf

--- app/components/Bookshelf.js
+++ app/components/Bookshelf.js
@@ -1,1 +1,32 @@
-Bookshelf.js
+import React from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+export default function Bookshelf ({ shelf, books, onMove }) {
+
+  const bookOnThisShelf = books.filter(book => book.shelf === shelf.key)
+
+  return (
+    <div className='bookshelf'>
+      <h2 className='bookshelf-title'>
+        {shelf.name}
+      </h2>
+      <div className='bookshelf-books'>
+        <ol className='books-grid'>
+          {bookOnThisShelf.map(book => (
+            <Book
+              key={book.id}
+              book={book}
+              shelf={shelf.key}
+            />
+          ))}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+Bookshelf.propTypes = {
+  shelf: PropTypes.object.isRequired,
+  books: PropTypes.array.isRequired
+}
