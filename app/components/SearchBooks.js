887064a03ca7a0aa1934bc19221c689d929a1b8b
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-26 20:11:31 -0400

    initiated SearchBooks component from BookIt to pass in onSearch onResetSearch onMove functions

--- app/components/SearchBooks.js
+++ app/components/SearchBooks.js
@@ -1,1 +1,43 @@
-SearchBooks.js
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import SearchResults from './SearchResults'
+import SearchBooksInput from './SearchBooksInput'
+
+export default function SearchBooks (
+  { searchBooks,
+    myBooks,
+    onSearch,
+    onResetSearch,
+    onMove }) {
+
+  return (
+    <div className='search-books'>
+      <div className='search-books-bar'>
+        <Link to="/">
+          <button
+            className='close-search'
+            onClick={onResetSearch}
+          >
+            Close
+          </button>
+        </Link>
+        <SearchBooksInput
+          onSearch={onSearch} />
+      </div>
+      <SearchResults
+        searchBooks={searchBooks}
+        myBooks={myBooks}
+        onMove={(onMove) => null}
+      />
+    </div>
+  )
+}
+
+SearchBooks.propTypes = {
+  searchBooks: PropTypes.array.isRequired,
+  myBooks: PropTypes.array.isRequired,
+  onSearch: PropTypes.func.isRequired,
+  onResetSearch: PropTypes.func.isRequired,
+  onMove: PropTypes.func.isRequired
+}
